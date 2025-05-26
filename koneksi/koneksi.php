7fe4bde4008005bbb9d82e46187dd209afc6d262
Eko Maulana Yusuf <eko.yusuf@dataon.com>
2025-05-26 20:25:08 +0700

    update koneksi

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,19 +1,15 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php'; // Sesuaikan path jika vendor ada di lokasi lain
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..'); // Sesuaikan path ke direktori root proyek
-$dotenv->load();
-
-    $user = $_ENV['DB_USER'];
-    $pass = $_ENV['DB_PASSWORD'];
-    $host = $_ENV['DB_HOST'];
-    $dbname = $_ENV['DB_NAME'];
+    $user =  'root';
+    $pass = 'password';
+    $host = 'localhost';
+    $dbname = 'rental_mobil';
 
     $koneksi = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
     global $url;
-    $url = "http://localhost:8080/";
+    $url = "http://localhost/rental_mobil/";
 
     $sql_web = "SELECT * FROM infoweb WHERE id = 1";
     $row_web = $koneksi->prepare($sql_web);
